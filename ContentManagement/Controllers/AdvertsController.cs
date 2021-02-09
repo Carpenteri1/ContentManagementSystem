893f8f129169c23d365a0a5f1a3dcb65e3c87416
Carpenteri1 <niclastimle@protonmail.com>
2021-02-09 15:47:52 +0100

    Layout changes

---
+++ ContentManagement/Controllers/AdvertsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentManagement.Controllers
+{
+    public class AdvertsController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+              return  Redirect("~/login");
+            }
+        }
+    }
+}
---
+++ ContentManagement/Models/Adverts/AdvertsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentManagement.Models.Adverts
+{
+    public class AdvertsModel
+    {
+        public int Id { get; set; }
+
+    }
+}
